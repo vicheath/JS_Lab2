7010154a1021f76d24ad3c13cf9a818561ee48b5
vicheath <vicheath.a.s@gmail.com>
2023-11-28 15:03:15 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
+
