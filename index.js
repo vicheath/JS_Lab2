464f6da6b1d10d231ec236564eaaf0191d1b1b08
vicheath <vicheath.a.s@gmail.com>
2023-11-28 15:35:41 -0500

    third commit

--- index.js
+++ index.js
@@ -4,3 +4,13 @@
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
 
+
+let x = 1
+
+while(x < 101){
+    if(x % 3 === 0){
+// Console.log(literal) = does not need to be declared - literaly spit this out
+    console.log('Fizz')
+    }
+    x++
+}
